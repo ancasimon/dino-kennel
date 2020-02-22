cbb2df65c3f8a0e546dc1c11f76515e32f884e81
Anca Simon <ancarsimon@gmail.com>
2020-02-22 11:34:05 -0600

    added View button and then Close button on single view dino function

--- main.js
+++ main.js
@@ -1,8 +1,61 @@
-const dinos = [];
+const dinos = [
+    {
+        id: 'dino1',
+        name: 'Rex',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Zoe',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+      },
+      {
+        id: 'dino2',
+        name: 'Jonah',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Luke',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRi6ZTKUpuygpYwvWTUbX_ck5OC3H-fohBR8-ifE21DcrToD6Cz'
+      },
+      {
+        id: 'dino3',
+        name: 'Frowns',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Mary',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://ichef.bbci.co.uk/wwfeatures/wm/live/624_351/images/live/p0/3j/mt/p03jmt44.jpg'
+      }
+];
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
+};
+
+// No - we start defining the single view: When you click the View/eye button on the all dinos page (we assign the same class to all these buttons and then add an event to all of them in the main initial printDionos function's domString - when clicked, they run the viewSingleDino function), then the kennel is emptied/an empty string gets pushed to the div with the 'kennel' id and a new printToDom function runs that creates the single dino view - see the viewSingleDino function. Then we also associate a close function (closeSingleViewEvent) with the Close/X button we have have added to the single View. 
+
+const closeSingleViewEvent = () => {
+    printToDom('single-view', '');
+    printDinos(dinos);
+};
+
+const viewSingleDino = () => {
+    let domString = '';
+    domString += '<button id="close-single-view" type="button" class="btn btn-outline-dark"><i class="fas fa-window-close"></i></button>';
+    printToDom('kennel', '');
+    printToDom('single-view', domString);
+    document.getElementById("close-single-view").addEventListener('click', closeSingleViewEvent);
+};
+
+const singleDinoAddEvents = () => {
+    const dinoViewButtons = document.getElementsByClassName('single-dino');
+    for (let i=0; i < dinoViewButtons.length; i++) {
+        dinoViewButtons[i].addEventListener('click', viewSingleDino);
+    };
 };
 
 const printDinos = (dinoArray) => {
@@ -14,11 +67,13 @@ const printDinos = (dinoArray) => {
         domString += '<div class="card-body">';
         domString += `<h2 class="card-title">${dinoArray[i].name}</h2>`;
         domString += `<p class="card-text">Health: ${dinoArray[i].health}</p>`;
+        domString += '<button class="btn btn-outline-dark single-dino"><i class="fas fa-eye"></i></button>';
         domString += '</div>';
         domString += '</div>';
         domString += '</div>';
     }
     printToDom("kennel", domString);
+    singleDinoAddEvents();
 };
 
 
@@ -62,6 +117,7 @@ const newDino = (e) => {
 // In the init function, we add the event listener for the click event - when user clicks the button with the submit-new-dino id, then the newDino function gets called. 
 const init = () => {
     document.getElementById('submit-new-dino').addEventListener('click', newDino);
+    printDinos(dinos);
 };
 
 init();
